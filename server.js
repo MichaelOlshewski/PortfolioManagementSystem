9f263b47f800dd37bbebbee01fb8083630da6886
Michael Olshewski <michael.olshewski@yahoo.com>
2020-10-14 21:03:48 -0400

    Setup React To Show At Deploy

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(express.static("public"))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "/client/public/index.html"))
+    res.sendFile(path.join(__dirname, "/index.html"))
 })
 
 app.listen(PORT, () => {
