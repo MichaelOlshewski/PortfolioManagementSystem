a2b8451cb497e447010e6eb7da6397d6e249c635
Michael Olshewski <michael.olshewski@yahoo.com>
2020-10-14 20:55:19 -0400

    Created New React App

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(express.static("public"))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "/client/build/index.html"))
+    res.sendFile(path.join(__dirname, "/client/public/index.html"))
 })
 
 app.listen(PORT, () => {
