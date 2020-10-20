e0355ad906e6a099ae3714ad02c11a56f9960971
Michael Olshewski <michael.olshewski@yahoo.com>
2020-10-20 14:35:46 -0400

    Heroku Fix

--- server.js
+++ server.js
@@ -16,8 +16,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
-app.use(cors())
-app.use(express.static("client/build"))
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static("client/build"))
+} else {
+    app.use(express.static("public"))
+}
 
 //Passport Middleware
 app.use(passport.initialize())
