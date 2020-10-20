a2a1c16bc2b8bd86230591dc801bb2aedd768e4e
Michael Olshewski <michael.olshewski@yahoo.com>
2020-10-20 16:07:03 -0400

    Heroku Fix

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
-if (process.env.NODE_ENV === 'production') {
+
+if (process.env.NODE_ENV === "production") {
     app.use(express.static('client/build'));
 }
 
