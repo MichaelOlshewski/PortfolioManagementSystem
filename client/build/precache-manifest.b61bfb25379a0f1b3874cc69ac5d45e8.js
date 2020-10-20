6bc0e23d635a559de99d5427b46ddd9c7a5c71c0
Michael Olshewski <michael.olshewski@yahoo.com>
2020-10-20 14:12:41 -0400

    Heroku Fix

---
+++ client/build/precache-manifest.b61bfb25379a0f1b3874cc69ac5d45e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57962f874bff317973074799f2c1b8a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c939b471dee227da4cd1",
+    "url": "/static/css/main.c80130b2.chunk.css"
+  },
+  {
+    "revision": "157df4366462781d4a91",
+    "url": "/static/js/2.4b9e8168.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.4b9e8168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c939b471dee227da4cd1",
+    "url": "/static/js/main.83870f1d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
