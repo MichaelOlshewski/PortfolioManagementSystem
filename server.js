9294c9bb859a6373d5aa17131b8b7d56f3f21c62
Michael Olshewski <michael.olshewski@yahoo.com>
2020-10-20 16:10:30 -0400

    Heroku Fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 const db = require('./config/keys').mongoURI;
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
