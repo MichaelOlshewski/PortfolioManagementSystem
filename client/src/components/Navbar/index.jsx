8bbddae31f0ad1bbc9c813bb54d936984eee1dfb
Michael Olshewski <michael.olshewski@yahoo.com>
2020-10-31 10:24:44 -0400

    Updated API to work on front-end

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import { AppBar, Toolbar, Typography, IconButton } from '@material-ui/core';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { MdMenu, MdExitToApp } from 'react-icons/md';
+import { MdExitToApp } from 'react-icons/md';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -36,11 +36,8 @@ function Navbar(props) {
     return (
         <AppBar className={classes.navColor} position="static">
             <Toolbar>
-                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                    <MdMenu/>
-                </IconButton>
                 <Typography variant="h6" className={classes.title}>
-                    Welcome, {props.name}
+                    {props.name}
                 </Typography>
                 <Link onClick={props.btnAction} className={classes.logoutBtn} to="/login"><MdExitToApp /></Link>
             </Toolbar>
--- client/src/pages/portfolio/index.js
+++ client/src/pages/portfolio/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
@@ -16,30 +16,45 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-const getPortData = () => {
-    axios.get("/api/portdata")
-        .then((response) => {
-            const data = response.data
-            console.log("data recieved")
-            console.log(data)
-        })
-        .catch(() => {
-            alert("error recieving data")
-        })
-}
+function FullWidthGrid(props, settingData) {
 
-function FullWidthGrid(props) {
+    const [portData, setPortData] = useState([]);
+    const [settings, setSettings] = useState([])
 
-    
     useEffect(() => {
         getPortData();
-    });
+        getPortSettings();
+    }, []);
+
+    const getPortData = () => {
+        axios.get("/api/portdata")
+            .then((response) => {
+                setPortData(response.data)
+                console.log("data recieved")
+                console.log(response.data)
+            })
+            .catch(() => {
+                alert("error recieving data")
+            })
+    }
+
+    const getPortSettings = () => {
+        axios.get("/api/settings")
+            .then((response) => {
+                setSettings(response.data[0])
+                console.log("settings recieved")
+                console.log(response.data[0])
+            })
+            .catch(() => {
+                alert("error recieving settings")
+            })
+    }
 
     const classes = useStyles();
 
     return (
         <React.Fragment>
-            <Navbar />
+            <Navbar name={settings.portName}/>
             <div className={classes.root} style={{ paddingTop: 10 }}>
                 <Grid container spacing={3}>
                     <Grid item md={6}>
@@ -50,55 +65,23 @@ function FullWidthGrid(props) {
                     <Grid item md={6}>
                         <Paper className={classes.paper}> About Projects</Paper>
                     </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <Paper className={classes.paper}>
-                            {" "}
-                            <a className="App 1" href="/">
-                                {" "}
-                                App 1{" "}
-                            </a>{" "}
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <Paper className={classes.paper}>
-                            <a className="App 1" href="/">
-                                {" "}
-                                App 2{" "}
-                            </a>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <Paper className={classes.paper}>
-                            <a className="App 1" href="/">
-                                {" "}
-                                App 3{" "}
-                            </a>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <Paper className={classes.paper}>
-                            <a className="App 1" href="/">
-                                {" "}
-                                App 4{" "}
-                            </a>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <Paper className={classes.paper}>
-                            <a className="App 1" href="/">
-                                {" "}
-                                App 5{" "}
-                            </a>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <Paper className={classes.paper}>
-                            <a className="App 1" href="/">
-                                {" "}
-                                App 6{" "}
-                            </a>
-                        </Paper>
-                    </Grid>
+                    {portData.map((project) => {
+                        return (
+                            <Grid key={project._id} item xs={12} sm={6}>
+                                <Paper className={classes.paper}>
+                                    <a className="App 1" target="_blank" href={project.deployedLink}>
+                                        {project.title}
+                                    </a>
+                                    <p>
+                                        {project.description}
+                                    </p>
+                                    <a target="_blank" href={project.deployedLink}>View Deployed Application</a>
+                                    {" "}
+                                    <a target="_blank" href={project.repoLink}>View Repository for App</a>
+                                </Paper>
+                            </Grid>
+                        )
+                    })}
                 </Grid>
             </div>
         </React.Fragment>
