044b21a9b8475823694d98d335e75594d2cc26a6
Michael Olshewski <michael.olshewski@yahoo.com>
2020-11-04 21:58:00 -0500

    Almost Ready For Release

--- client/src/pages/portfolio/index.js
+++ client/src/pages/portfolio/index.js
@@ -11,26 +11,25 @@ import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        flexGrow: 1
+        flexGrow: 1,
     },
     paper: {
         padding: theme.spacing(2),
         textAlign: "center",
         color: theme.palette.text.secondary,
         marginLeft: 25,
-        marginRight: 25
+        marginRight: 25,
     },
     card: {
-    marginLeft: 25,
-   marginRight: 25,
-   textAlign: "center"
-}
+        marginLeft: 25,
+        marginRight: 25,
+        textAlign: "center",
+    },
 }));
 
 function FullWidthGrid(props, settingData) {
-
     const [portData, setPortData] = useState([]);
-    const [settings, setSettings] = useState([])
+    const [settings, setSettings] = useState([]);
 
     useEffect(() => {
         getPortData();
@@ -38,71 +37,96 @@ function FullWidthGrid(props, settingData) {
     }, []);
 
     const getPortData = () => {
-        axios.get("/api/portdata")
+        axios
+            .get("/api/portdata")
             .then((response) => {
-                setPortData(response.data)
+                setPortData(response.data);
+                console.log(response.data)
             })
             .catch(() => {
-                alert("error recieving data")
-            })
-    }
+                alert("error recieving data");
+            });
+    };
 
     const getPortSettings = () => {
-        axios.get("/api/settings")
+        axios
+            .get("/api/settings")
             .then((response) => {
-                setSettings(response.data[0])
+                setSettings(response.data[0]);
+                console.log(response.data)
             })
             .catch(() => {
-                alert("error recieving settings")
-            })
-    }
+                alert("error recieving settings");
+            });
+    };
 
     const classes = useStyles();
 
     return (
         <React.Fragment>
-            <Navbar name={settings.portName} linkedinLink={settings.linkedinLink} githubLink={settings.githubLink} />
+            <Navbar
+                name={settings.portName}
+                linkedinLink={settings.linkedinLink}
+                githubLink={settings.githubLink}
+            />
             <div className={classes.root} style={{ paddingTop: 10 }}>
                 <Grid container spacing={3}>
                     <Grid item md={12}>
-                    <Card className={classes.card}>
-                
-        <CardMedia
-          className={classes.media}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-           Name
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            This about me/about portfolio will update.
-          </Typography>
-        </CardContent>
-    </Card>
-    </Grid>
-    
+                        <Card className={classes.card}>
+                            <CardMedia
+                                className={classes.media}
+                                image="/static/images/cards/contemplative-reptile.jpg"
+                                title="Contemplative Reptile"
+                            />
+                            <CardContent>
+                                <Typography
+                                    gutterBottom
+                                    variant="h5"
+                                    component="h2"
+                                >
+                                    {settings.portUserName}
+                                </Typography>
+                                <Typography
+                                    variant="body2"
+                                    color="textSecondary"
+                                    component="p"
+                                >
+                                    {settings.portAbout}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
 
                     {portData.map((project) => {
                         return (
                             <Grid key={project._id} item xs={12} sm={6}>
                                 <Paper className={classes.paper}>
-                                    <a rel="noopener noreferrer" target="_blank" href={project.deployedLink}>
+                                    <a
+                                        rel="noopener noreferrer"
+                                        target="_blank"
+                                        href={project.deployedLink}
+                                    >
                                         {project.title}
                                     </a>
-                                    <p>
-                                        {project.image}
-                                    </p>
-                                    <p>
-                                        {project.description}
-                                    </p>
-                                    <a target="_blank" rel="noopener noreferrer" href={project.deployedLink}>View Deployed Application</a>
-                                    {" "}
-                                    <a target="_blank" rel="noopener noreferrer" href={project.repoLink}>View Repository for App</a>
+                                    <p>{project.image}</p>
+                                    <p>{project.description}</p>
+                                    <a
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        href={project.deployedLink}
+                                    >
+                                        View Deployed Application
+                                    </a>{" "}
+                                    <a
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        href={project.repoLink}
+                                    >
+                                        View Repository for App
+                                    </a>
                                 </Paper>
                             </Grid>
-                        )
+                        );
                     })}
                 </Grid>
             </div>
