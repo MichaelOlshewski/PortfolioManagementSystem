83f70ca31010758be0d3bb59755a989d41042f3b
Allison Barnard <allisonbarnard07@gmail.com>
2020-11-04 20:15:57 -0500

    Design changes

--- client/src/pages/portfolio/index.js
+++ client/src/pages/portfolio/index.js
@@ -4,16 +4,28 @@ import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Navbar from "../../components/Navbar";
 import axios from "axios";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
+        backgroundColor: "#99FFFF"
     },
     paper: {
         padding: theme.spacing(2),
         textAlign: "center",
         color: theme.palette.text.secondary,
+        marginLeft: 25,
+        marginRight: 25
     },
+    card: {
+    marginLeft: 25,
+   marginRight: 25,
+   textAlign: "center"
+}
 }));
 
 function FullWidthGrid(props, settingData) {
@@ -53,14 +65,26 @@ function FullWidthGrid(props, settingData) {
             <Navbar name={settings.portName} linkedinLink={settings.linkedinLink} githubLink={settings.githubLink} />
             <div className={classes.root} style={{ paddingTop: 10 }}>
                 <Grid container spacing={3}>
-                    <Grid item md={6}>
-                        <Paper className={classes.paper}>
-                            Image for Portfolio & Social Links{" "}
-                        </Paper>
-                    </Grid>
-                    <Grid item md={6}>
-                        <Paper className={classes.paper}> About Projects</Paper>
-                    </Grid>
+                    <Grid item md={12}>
+                    <Card className={classes.card}>
+                
+        <CardMedia
+          className={classes.media}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+           Name
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            This about me/about portfolio will update.
+          </Typography>
+        </CardContent>
+    </Card>
+    </Grid>
+    
+
                     {portData.map((project) => {
                         return (
                             <Grid key={project._id} item xs={12} sm={6}>
